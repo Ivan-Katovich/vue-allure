bad80b4432e4ce4f1a724a543ae9b2d34e83048e
Ivan Kotovich <kotovich.ivan@gmail.com>
2020-04-06 13:33:21 +0300

    charts into components

---
+++ src/components/CategoryChart.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="chart" v-if="this.series[0].data.length">
+        <apexchart :options="options" :series="series"></apexchart>
+    </div>
+</template>
+
+<script>
+    import VueApexCharts from 'vue-apexcharts';
+
+    export default {
+        name: "CategoryChart",
+        props: {
+            smartCategories: Array,
+            wait: Promise
+        },
+        data() {
+            return {
+                series: [{name: 'defects', data: []}],
+                options: {
+                    chart: {
+                        id: 'defects',
+                        type: 'bar',
+                        height: 350
+                    },
+                    title: {
+                        text: 'Defects categories',
+                        align: 'center',
+                        floating: true,
+                        offsetY: -7,
+                        style: {
+                            fontSize:  '20px',
+                            fontWeight:  'bold',
+                            fontFamily:  undefined,
+                            color:  '#373d3f'
+                        },
+                    },
+                    colors: ['#800000'],
+                    plotOptions: {
+                        bar: {
+                            horizontal: true,
+                        }
+                    },
+                    dataLabels: {
+                        enabled: true
+                    },
+                    yaxis: {
+                        forceNiceScale: true,
+                        labels: {
+                            style: {
+                                fontSize: '14px'
+                            }
+                        }
+                    },
+                    xaxis: {
+                        categories: [],
+                        labels: {
+                            style: {
+                                fontSize: '14px'
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        components: {
+            apexchart: VueApexCharts,
+        },
+        async mounted() {
+            await this.wait;
+            this.series[0].data = this.smartCategories.map((category) => category.statistic.total);
+            this.options = {...this.options, ... {
+                    xaxis: {
+                        categories: this.smartCategories.map((category) => category.name)
+                    }}};
+        }
+    }
+</script>
+
+<style scoped>
+    .chart {
+        width: 80%;
+        margin-left: auto;
+        margin-right: auto;
+    }
+</style>
--- src/components/SuiteForm.vue
+++ src/components/SuiteForm.vue
@@ -3,8 +3,8 @@
         <div class="title">
             <h1 class="center" v-if="env.values">{{title}} - {{env.values[0]}}</h1>
         </div>
-        <div class="chart">
-            <apexchart :options="chartOptions" :series="chartSeries"></apexchart>
+        <div class="results">
+            <results-chart :smart-results="this.smartResults" :wait="this.waitData"/>
         </div>
         <div class="time frame">
             <time-box :smart-time="this.smartTime"/>
@@ -14,27 +14,30 @@
                 </a>
             </div>
         </div>
-        <svg class="border">
-            <rect class="line" />
-        </svg>
+        <div>
+            <svg class="border">
+                <rect class="line" />
+            </svg>
+        </div>
         <div class="info frame">
             <info-box :smart-info="this.smartInfo"/>
         </div>
-        <svg class="border">
-            <rect class="line" />
-        </svg>
+        <div>
+            <svg class="border">
+                <rect class="line" />
+            </svg>
+        </div>
         <div class="categories">
-            <div id="chart" class="chart">
-                <apexchart :options="categoryOptions" :series="categorySeries"></apexchart>
-            </div>
+            <category-chart :smart-categories="this.smartCategories" :wait="this.waitData"/>
         </div>
     </div>
 </template>
 
 <script>
-    import VueApexCharts from 'vue-apexcharts';
     import TimeBox from './TimeBox';
     import InfoBox from './InfoBox';
+    import CategoryChart from './CategoryChart';
+    import ResultsChart from './ResultsChart';
     import moment from 'moment';
     import {getSummary, getCategories, getEnvInfo} from '../support/allureDataGetter';
 
@@ -56,118 +59,15 @@
                 smartTime: {},
                 smartInfo: [],
                 smartCategories: [],
-                chartSeries: [],
-                chartOptions: {
-                    labels: [' passed', ' failed', ' skipped'],
-                    colors:['#336600', '#800000', '#7c7e7a'],
-                    legend: {
-                        position: 'right',
-                        offsetY: 0,
-                        height: 400,
-                        fontSize: '20px',
-                        markers: {
-                            width: 14,
-                            height: 14,
-                            strokeWidth: 0,
-                            strokeColor: '#fff',
-                            fillColors: undefined,
-                            radius: 12,
-                            customHTML: undefined,
-                            onClick: undefined,
-                            offsetX: 0,
-                            offsetY: 0
-                        },
-                    },
-                    chart: {
-                        id: 'statistic',
-                        type: 'donut',
-                    },
-                    plotOptions: {
-                        pie: {
-                            donut: {
-                                size: '75%',
-                                labels: {
-                                    show: true,
-                                    total: {
-                                        label: 'Total TCs run',
-                                        showAlways: true,
-                                        show: true,
-                                        fontSize: '150%',
-                                    },
-                                    value: {
-                                        show: true,
-                                        fontSize: '200%',
-                                        offsetY: 25,
-                                    },
-                                }
-                            }
-                        }
-                    },
-                    responsive: [{
-                        breakpoint: 480,
-                        options: {
-                            chart: {
-                                width: 10
-                            },
-                            legend: {
-                                position: 'bottom'
-                            }
-                        }
-                    }]
-                },
-                categorySeries: [{name: 'defects', data: []}],
-                categoryOptions: {
-                    chart: {
-                        id: 'defects',
-                        type: 'bar',
-                        height: 350
-                    },
-                    title: {
-                        text: 'Defects categories',
-                        align: 'center',
-                        floating: true,
-                        offsetY: -7,
-                        style: {
-                            fontSize:  '20px',
-                            fontWeight:  'bold',
-                            fontFamily:  undefined,
-                            color:  '#373d3f'
-                        },
-                    },
-                    colors: ['#800000'],
-                    plotOptions: {
-                        bar: {
-                            horizontal: true,
-                        }
-                    },
-                    dataLabels: {
-                        enabled: true
-                    },
-                    yaxis: {
-                        forceNiceScale: true,
-                        labels: {
-                            style: {
-                                fontSize: '14px'
-                            }
-                        }
-                    },
-                    xaxis: {
-                        categories: [],
-                        labels: {
-                            style: {
-                                fontSize: '14px'
-                            }
-                        }
-                    }
-                },
                 env: {},
                 waitData: null
             }
         },
         components: {
-            apexchart: VueApexCharts,
             TimeBox,
-            InfoBox
+            InfoBox,
+            CategoryChart,
+            ResultsChart
         },
         methods: {
             async organizeData() {
@@ -195,36 +95,20 @@
                 this.smartCategories = categories;
             },
             applyData() {
-                this.chartOptions = {...this.chartOptions, ...{
-                    labels: Object.keys(this.smartResults)
-                    }};
-                this.chartSeries = Object.values(this.smartResults);
-                this.categorySeries[0].data = this.smartCategories.map((category) => category.statistic.total);
-                this.categoryOptions = {...this.categoryOptions, ... {
-                    xaxis: {
-                        categories: this.smartCategories.map((category) => category.name)
-                    }}};
                 this.env = this.smartInfo.find((i) => i.name === 'Environment');
             }
         },
         async created() {
             this.waitData = this.organizeData();
         },
-        mounted() {
-            this.waitData
-                .then(() => {
-                    this.applyData();
-                });
+        async mounted() {
+            await this.waitData;
+            this.applyData();
         }
     }
 </script>
 
 <style scoped>
-    .chart {
-        width: 80%;
-        margin-left: auto;
-        margin-right: auto;
-    }
     .title {
         height: 100px;
         text-align: center;
